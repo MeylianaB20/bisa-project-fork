f933260e74635b8a4e67516ffcbc2e94059133f1
Bourne Dhiaze <bourne.jo@binus.edu>
2024-05-26 18:42:05 +0700

    #MAIN - Login Disable Button

--- screens/home/HomeScreen.js
+++ screens/home/HomeScreen.js
@@ -3,11 +3,13 @@ import Header from "../../components/home/Header";
 import Banner from "../../components/home/Banner/Banner";
 import Category from "../../components/home/Category/Category";
 
-const HomeScreen = () => {
+const HomeScreen = ({route}) => {
+  const email = route.params.email;
+
   return (
     <SafeAreaView style={styles.container}>
       <ScrollView>
-        <Header />
+        <Header email={email}/>
         <Banner />
         <Category />
       </ScrollView>
