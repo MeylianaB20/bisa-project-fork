6f8b9762f07ad7b77502fdd6ac897324d39c000c
Bourne Dhiaze <bourne.jo@binus.edu>
2024-05-26 22:18:41 +0700

    #MAIN - Mentor

--- screens/login/LoginScreen.js
+++ screens/login/LoginScreen.js
@@ -1,11 +1,14 @@
-import { StyleSheet, View } from "react-native";
+import { StyleSheet, View, TouchableOpacity, Text, Dimensions } from "react-native";
 import Header from "../../components/auth/Header";
 import InputField from "../../components/auth/InputField";
 import SignInButton from "../../components/auth/SignInButton";
 import SignInBreaker from "../../components/auth/SignInBreaker";
 import WindowsLogin from "../../components/auth/WindowsLogin";
+import { useNavigation } from "@react-navigation/native";
 
 const LoginScreen = ({ handleUser, user }) => {
+  const navigation = useNavigation();
+
   return (
     <View style={styles.layout}>
       <Header />
@@ -13,6 +16,11 @@ const LoginScreen = ({ handleUser, user }) => {
       <SignInButton user={user} />
       <SignInBreaker />
       <WindowsLogin />
+      {/* <TouchableOpacity onPress={() => navigation.navigate('Mentor')} style={styles.developButton}>
+        <View style={{justifyContent: "center"}}>
+          <Text>Develop</Text>
+        </View>
+      </TouchableOpacity> */}
     </View>
   );
 };
@@ -21,6 +29,16 @@ const styles = StyleSheet.create({
   layout: {
     backgroundColor: "white",
     height: "100%",
+  },
+  developButton: {
+    backgroundColor: "lightblue",
+    width: Dimensions.get("window").width - 48,
+    height: 43,
+    marginTop: 25,
+    marginHorizontal: 24,
+    borderRadius: 4,
+    justifyContent: "center",
+    alignItems: "center",
   },
 });
 
